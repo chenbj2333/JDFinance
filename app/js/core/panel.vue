816accfbfae7aeaead3f1cbfeb6bdcd2f93e8f75
chenbj2333 <1749402844@qq.com>
2018-05-24 17:31:56 +0800

    公共组件（未完）

---
+++ app/js/core/panel.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="panel">
+    <h4>{{title}}</h4>
+    <div>{{content}}</div>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    title: {type: String, default: ""},
+    content: {type: String, default: ""}
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "../../css/element.scss";
+  .panel {
+    @include panel;
+  }
+</style>
